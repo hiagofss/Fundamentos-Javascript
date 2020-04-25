754b620d1b163bc5d80461a536abeaf3f73052c8
Hiago Souza <hiagofss98@gmail.com>
2020-04-25 12:39:12 -0300

    Modulo-> Iniciando com Javascript

---
+++ iniciando-com-javascript/variaveis.js
@@ -0,0 +1,19 @@
+// Let é uma variavel que pode ser re-assinada
+let nome = 'Super Man';
+let idade = 15;
+let poder = 'Super força';
+nome = 'Spider Man';
+
+// Const usado para valores que não se alteram
+const ativo = true;
+
+// Var não deve ser usada mais
+var pais = 'Brasil';
+
+console.log(
+  `
+  Nome: ${nome}
+  Idade: ${idade}
+  Poder: ${poder}
+  `
+);
